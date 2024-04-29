a5d4dfd3c0bfaca1606fe086a24459602e0a1ca9
Deuces9ers <157613040+Deuces9ers@users.noreply.github.com>
2024-04-29 05:34:13 -0700

    Update ConstructorForwarderOxCD84597aD4eb60FF346b54395bde47B878e76F13

--- l1-contracts/contracts/dev-contracts/ConstructorForwarder.sol
+++ l1-contracts/contracts/dev-contracts/ConstructorForwarder.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.20;
 
-contract ConstructorForwarder {
+contract ConstructorForwarder OxCD84597aD4eb60FF346b54395bde47B878e76F13 {
     constructor(addressOxCD84597aD4eb60FF346b54395bde47B878e76F13 to, bytes memory data) payable {
         (bool success, ) = payable(to).call{value: msg.value}(data);
         require(success);
